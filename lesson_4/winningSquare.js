d8fc6f8f7080567c38c1f8ed17891f86b33fbd53
Michael Singhurse <michaelsinghurse@gmail.com>
2020-02-24 15:34:45 +0000

    refactored implementation

--- lesson_4/winningSquare.js
+++ lesson_4/winningSquare.js
@@ -15,48 +15,31 @@ const POSSIBLE_WINNING_ROWS = [
   [ "3", "5", "7" ],            // diagonal: bottom-left to top-right
 ];
 
-function winningSquare(board, marker) {
-  let numOfMarkers = 0;
-  let numOfEmptys = 0;
-  let emptyKey = '';
+function winningSquare(squares, marker) {
   
   for (let index = 0; index < POSSIBLE_WINNING_ROWS.length; index += 1) {
-    let winning_row = POSSIBLE_WINNING_ROWS[index];
+    let winningRow = POSSIBLE_WINNING_ROWS[index];
     
-    winning_row.forEach(square => {
-      if (board[square] === marker) {
-        numOfMarkers += 1;
-      } else if (board[square] === EMPTY_MARKER) {
-        numOfEmptys += 1;
-        emptyKey = square;
-      }
-    });
-    
-    if (numOfMarkers === 2 && numOfEmptys === 1) {
-      return emptyKey;
+    if (winningRow.filter(key => squares[key] === marker).length === 2 &&
+        winningRow.filter(key => squares[key] === EMPTY_MARKER).length === 1) {
+        return winningRow.find(key => squares[key] === EMPTY_MARKER);
     }
-    
-    numOfMarkers = 0;
-    numOfEmptys = 0;
   }
-  
+
   return null;
 }
 
-let squares = {
-  1: 'O',
-  2: 'X',
+let board = {
+  1: 'X',
+  2: ' ',
   3: ' ', 
   4: ' ',
-  5: ' ',
-  6: ' ',
+  5: 'X',
+  6: 'O',
   7: ' ',
   8: ' ',
-  9: ' ',
+  9: 'O',
 };
 
-let winner = winningSquare(squares, COMPUTER_MARKER);
+let winner = winningSquare(board, COMPUTER_MARKER);
 console.log(winner);
-
-
-
