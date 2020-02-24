1d9ebc38b71cefa00493d426fd8726a70080f834
Michael Singhurse <michaelsinghurse@gmail.com>
2020-02-24 18:17:56 +0000

    added scaffolding and stubs and a spike

---
+++ lesson_4/oo_twentyone.js
@@ -0,0 +1,147 @@
+// oo_twentyone.js
+
+class Card {
+  constructor() {
+    // STUB
+    // What sort of state does a card need?
+    // Rank? Suit? Points?
+  }
+}
+
+class Deck {
+  constructor() {
+    // STUB
+    // What sort of state does a deck need?
+    // 52 cards
+    // What data structure to keep track of cards
+    // - array, object, or something else?
+  }
+
+  deal() {
+    // STUB
+    // does the Dealer or the Deck deal?
+  }
+}
+
+class Participant {
+  constructor() {
+    // STUB
+    // What sort of state does a participant need?
+    // Score? Hand? Amount of money available?
+    // What else goes here? All the redundant behavior from Player and Dealer?
+  }
+}
+
+class Player extends Participant {
+  constructor() {
+    // STUB
+    // What sort of state does a player need?
+    // Score? Hand? Amount of money available?
+  }
+
+  hit() {
+    // STUB
+  }
+
+  stay() {
+    // STUB
+  }
+
+  isBusted() {
+    // STUB
+  }
+
+  score() {
+    // STUB
+  }
+}
+
+class Dealer extends Participant {
+  // Very similar to a Player. Do we need this?
+
+  constructor() {
+    // STUB
+    // What sort of state does a dealer need?
+    // Score? Hand? Deck of cards?
+  }
+
+  hit() {
+    // STUB
+  }
+
+  stay() {
+    // STUB
+  }
+
+  isBusted() {
+    // STUB
+  }
+
+  score() {
+    // STUB
+  }
+
+  hide() {
+    // STUB
+  }
+
+  reveal() {
+    // STUB
+  }
+
+  deal() {
+    // STUB
+    // does the dealer or the deck deal?
+  }
+}
+
+class TwentyOneGame {
+  constructor() {
+    // STUB
+    // What sort of state does a game need?
+    // A deck? Two participants?
+  }
+
+  dealCards() {
+    // STUB
+  }
+
+  dealerTurn() {
+    // STUB
+  }
+
+  displayGoodbyeMessage() {
+    // STUB
+  }
+
+  displayResult() {
+    // STUB
+  }
+
+  displayWelcomeMessage() {
+    // STUB
+  }
+
+  playerTurn() {
+    // STUB
+  }
+
+  showCards() {
+    // STUB
+  }
+
+  start() {
+    // SPIKE
+    this.displayWelcomeMessage();
+    this.dealCards();
+    this.showCards();
+    this.playerTurn();
+    this.dealerTurn();
+    this.displayResult();
+    this.displayGoodbyeMessage();
+  }
+}
+
+let game = new TwentyOneGame();
+game.start();
+
